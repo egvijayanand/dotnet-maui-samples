e235b5247500a2223351429bff739b7115d0ddc0
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2021-08-02 20:03:31 +0530

    Sample updated to Preview 6 changes

---
+++ src/TestApp/TestApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace TestApp
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate<Startup>
+    {
+    }
+}
