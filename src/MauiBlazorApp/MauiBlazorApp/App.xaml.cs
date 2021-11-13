40840fb39126f4a5caf89163e6a9cad17c20ec98
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2021-11-13 20:31:27 +0530

    .NET MAUI Blazor Sample

---
+++ src/MauiBlazorApp/MauiBlazorApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace MauiBlazorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // C# definition
+            MainPage = new BlazorPage();
+
+            // XAML definition
+            //MainPage = new WebPage();
+        }
+    }
+}
---
+++ src/MauiBlazorApp/MauiBlazorApp/BlazorPage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Controls;
+
+namespace MauiBlazorApp
+{
+    public class BlazorPage : ContentPage
+    {
+        public BlazorPage()
+        {
+            SetDynamicResource(BackgroundColorProperty, "SecondaryColor");
+
+            var blazor = new BlazorWebView()
+            {
+                HostPage = "wwwroot/index.html"
+            };
+
+            blazor.RootComponents.Add(new()
+            {
+                ComponentType = typeof(Gateway),
+                Selector = "#app"
+            });
+
+            Content = blazor;
+        }
+    }
+}
