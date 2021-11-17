7211e6908f927059037c3ec40b9776d3b37ef73e
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2021-11-17 16:06:11 +0530

    Added extension methods for BlazorWebView

---
+++ src/MauiBlazorApp/MauiBlazorApp/Extensions/BlazorExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using System;
+using System.Collections.Generic;
+
+namespace MauiBlazorApp.Extensions
+{
+    public static class BlazorExtensions
+    {
+        public static TBlazorWebView HostPage<TBlazorWebView>(this TBlazorWebView blazorWV, string hostPage)
+            where TBlazorWebView : BlazorWebView
+        {
+            blazorWV.HostPage = hostPage;
+            return blazorWV;
+        }
+
+        public static TRootComponent ComponentType<TRootComponent>(this TRootComponent component, Type type)
+            where TRootComponent : RootComponent
+        {
+            component.ComponentType = type;
+            return component;
+        }
+
+        public static TRootComponent Selector<TRootComponent>(this TRootComponent component, string selector)
+            where TRootComponent : RootComponent
+        {
+            component.Selector = selector;
+            return component;
+        }
+
+        public static TRootComponent Parameters<TRootComponent>(this TRootComponent component, params (string key, object? value)[] parameters)
+            where TRootComponent : RootComponent
+        {
+            var dict = new Dictionary<string, object?>();
+
+            foreach (var (key, value) in parameters)
+            {
+                dict.Add(key, value);
+            }
+
+            component.Parameters = dict;
+            return component;
+        }
+    }
+}
