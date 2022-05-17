3368195e60fe84840b2ce0c7a2fbfb9112629ddf
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-05-17 18:29:14 +0530

    Adding a C# Markup based Sample app

---
+++ src/MauiAppCS/MauiAppCS/Imports.cs
@@ -0,0 +1,7 @@
+﻿// Markup
+global using CommunityToolkit.Maui.Markup;
+global using VijayAnand.MauiToolkit.Markup;
+global using static CommunityToolkit.Maui.Markup.GridRowsColumns;
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
+global using static VijayAnand.MauiToolkit.Markup.Utility;
---
+++ src/MauiAppCS/MauiAppCS/Resources/Styles.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppCS
+{
+    public partial class Styles : ResourceDictionary
+    {
+        public Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}
