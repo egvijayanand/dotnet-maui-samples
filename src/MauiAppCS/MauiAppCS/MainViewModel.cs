633177bb06cc07b6e0187cd555e5322253de0aa9
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-06-07 23:33:00 +0530

    Converted as MVVM sample and updated MVVM NuGet package to latest preview release

---
+++ src/MauiAppCS/MauiAppCS/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiAppCS
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private int count;
+        private readonly ISemanticScreenReader semanticScreenReader;
+
+        public MainViewModel(ISemanticScreenReader semanticScreenReader)
+        {
+            this.semanticScreenReader = semanticScreenReader;
+        }
+
+        [ObservableProperty]
+        private string counter = "Current count: 0";
+
+        [RelayCommand]
+        private void OnClick()
+        {
+            count++;
+            Counter = $"Current count: {count}";
+            semanticScreenReader.Announce(Counter);
+        }
+    }
+}
--- src/MauiAppCS/MauiAppCS/MauiProgram.cs
+++ src/MauiAppCS/MauiAppCS/MauiProgram.cs
@@ -13,6 +13,9 @@
                        fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
                    });
 
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton(SemanticScreenReader.Default);
             return builder.Build();
         }
     }
