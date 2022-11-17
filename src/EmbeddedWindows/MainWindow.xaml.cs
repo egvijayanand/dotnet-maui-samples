b61dbfc97c293e92e849fbb86027e90865de2ff9
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-11-18 00:55:39 +0530

    Embedded Windows sample tryout

---
+++ src/EmbeddedWindows/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+using CommunityToolkit.Maui.Markup;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
+using Microsoft.UI.Xaml;
+
+namespace EmbeddedWindows
+{
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			var mauiApp = MauiApp.CreateBuilder()
+								 .UseMauiApp<Microsoft.Maui.Controls.Application>()
+								 .UseMauiCommunityToolkitMarkup()
+								 .Build();
+			var mauiContext = new MauiContext(mauiApp.Services);
+			var mauiPage = new MauiPage();
+			Content = mauiPage.ToPlatform(mauiContext);
+		}
+	}
+}
---
+++ src/EmbeddedWindows/MauiPage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using CommunityToolkit.Maui.Markup;
+using static Microsoft.Maui.Graphics.Colors;
+
+namespace EmbeddedWindows
+{
+	public class MauiPage : ContentPage
+	{
+		public MauiPage()
+		{
+			Content = new Grid
+			{
+				Children =
+				{
+					new Label
+					{
+						Text = "Hello .NET MAUI!!!"
+					}.Center().TextColor(Purple)
+				}
+			};
+		}
+	}
+}
