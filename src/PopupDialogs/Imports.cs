74b24ebd2ada982a2ab23bd444eec41a162c4d31
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-12-29 05:38:21 +0530

    Custom dialog implemented with VijayAnand.MauiToolkit.Pro NuGet package

---
+++ src/PopupDialogs/Imports.cs
@@ -0,0 +1,7 @@
+global using VijayAnand.MauiToolkit;
+global using VijayAnand.MauiToolkit.Core.Services;
+global using VijayAnand.MauiToolkit.Pro;
+global using VijayAnand.MauiToolkit.Pro.Services;
+
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
---
+++ src/PopupDialogs/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using VijayAnand.MauiToolkit.Core;
+
+namespace PopupDialogs
+{
+    public partial class MainPage : ContentPage
+    {
+        readonly IDialogService _dialogService;
+
+        public MainPage(IDialogService dialogService)
+        {
+            InitializeComponent();
+            _dialogService = dialogService;
+        }
+
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                // DI approach
+                // Switch betwen native/custom implementations in the MauiProgram.cs
+                // The order in which dependencies are registered determines the instance injected
+                // The latest one wins
+
+                switch (btn.AutomationId)
+                {
+                    case "Alert1":
+                        await _dialogService.DisplayAlertAsync("Greeting", "Hello from .NET MAUI!!!", "OK");
+                        break;
+                    case "Alert2":
+                        var response = await _dialogService.DisplayAlertAsync("Confirm", "Would you like to proceed?", "Yes", "No");
+                        var message = response is true ? "Glad that you opted in." : "Sorry to miss you, hope to see you soon.";
+                        await _dialogService.DisplayAlertAsync("Response", message, "OK");
+                        break;
+                    case "Prompt":
+                        var result = await _dialogService.DisplayPromptAsync("Subscribe", "Enter your email to send notifications.", maxLength: 100, inputType: InputType.Email);
+                        
+                        if (!string.IsNullOrWhiteSpace(result))
+                        {
+                            await _dialogService.DisplayAlertAsync("Response", result, "OK");
+                        }
+                        break;
+                    case "ActionSheet":
+                        var action = await _dialogService.DisplayActionSheetAsync("Unsaved Changes ...", "What would you like to do?", "Cancel", "Discard", "Save", "Save", "Upload", "Share", "Discard");
+                        await _dialogService.DisplayAlertAsync("Response", action, "OK");
+                        break;
+                    default:
+                        // Static approach for custom implementation
+                        // Static approach for native dialogs soon in an upcoming preview
+                        await PopupDialog.Instance.DisplayAlertAsync("Static Invocation", "Welcome to .NET MAUI!!!", "OK");
+                        break;
+                }
+            }
+        }
+    }
+}
