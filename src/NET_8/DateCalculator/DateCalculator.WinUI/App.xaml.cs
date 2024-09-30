6711b688db3f78c3f69eb56075a94848a86a4afa
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2024-09-30 23:45:50 +0530

    Frame-based navigation and MVVM Toolkit temporary fix

--- src/NET_8/DateCalculator/DateCalculator.WinUI/App.xaml.cs
+++ src/NET_8/DateCalculator/DateCalculator.WinUI/App.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Navigation;
+
 namespace DateCalculator.WinUI
 {
     /// <summary>
@@ -5,6 +7,8 @@ namespace DateCalculator.WinUI
     /// </summary>
     public partial class App : Application
     {
+        private Window window = Window.Current;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -18,13 +22,30 @@ namespace DateCalculator.WinUI
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            m_window = new MainWindow();
-            m_window?.Activate();
+            window ??= new Window();
+
+            if (window.Content is not Frame rootFrame)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                window.Content = rootFrame;
+            }
+
+            _ = rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            window.Activate();
         }
 
-        private Window? m_window;
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
     }
 }
